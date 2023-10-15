00a040cb2f6a53cad5ec3e1fcb9909966166c354
phamhoangngocquan2805 <phamhoangngocquan2805@gmail.com>
2023-10-15 16:01:27 +0700

    push bttuan05

---
+++ baiTapTuan05/screen_1.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function screen_1() {
+  return (
+    <View>
+      <Text>screen_1</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
